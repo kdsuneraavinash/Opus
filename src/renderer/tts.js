d5f8433e93bfdc47aba6110c74b61d0b21806af4
Budvin Chathura <chathura97rc@gmail.com>
2021-05-30 02:31:32 +0530

    Add OS specific commands for TTS

--- src/renderer/tts.js
+++ src/renderer/tts.js
@@ -1,64 +1,86 @@
-const quill = require("./quill");
+const quill = require('./quill');
 const { exec } = require('child_process');
+const os = require('os');
+
 
 const ttsButton = document.querySelector('.tts-button');
 
-var readingAloud = false;
-var readerPID = -1;
+let readingAloud = false;
+let readerPID = -1;
 
 async function readApi(text) {
-    return new Promise(resolve => {
-        readerPID = exec(`konsole --hold -e cowsay "${text}"`, (err, stdout, stderr) => {
-            resolve();
-        }).pid;
-    });
+  text = text.replace('\'', ' ');
+  text = text.replace('"', ' ');
+
+  let command;
+  switch (os.platform()) {
+    case 'win32':
+      command = `PowerShell -Command "Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('${text}');"`;
+      break;
+    case 'darwin':
+      command = `say "${text}"`;
+      break;
+
+    case 'linux':
+      command = `spd-say "${text}"`;
+      break;
+
+    default:
+      throw new Error('OS not supported for text-to-speech feature.');
+  }
+  return new Promise((resolve) => {
+    readerPID = exec(command, (err, stdout, stderr) => {
+      resolve();
+    }).pid;
+  });
 }
 
 module.exports = {
-    init() {
-        ttsButton.addEventListener('click', () => {
-            if (readingAloud) {
-                this.stop();
-            } else {
-                this.read(quill.getText());
-            }
-        });
-        this.setState(false);
-    },
+  init() {
+    ttsButton.addEventListener('click', () => {
+      if (readingAloud) {
+        this.stop();
+      } else {
+        this.read(quill.getText());
+      }
+    });
+    this.setState(false);
+  },
 
-    isReading() {
-        return readingAloud;
-    },
+  isReading() {
+    return readingAloud;
+  },
 
-    setState(reading) {
-        readingAloud = reading;
-        if (reading) {
-            ttsButton.innerHTML = "🔈 STOP";
-        } else {
-            readerPID = -1;
-            ttsButton.innerHTML = "🔊  TTS";
-        }
-    },
-
-    async read(text) {
-        if (this.isReading()) {
-            this.stop();
-            this.setState(false);
-        } else {
-            this.setState(true);
-            await readApi(text);
-            this.setState(false);
-        }
-    },
-
-    stop() {
-        if (readerPID == -1) {
-            this.setState(false);
-        } else {
-            try {
-                process.kill(readerPID);
-            } catch (e) {
-            }
-        }
+  setState(reading) {
+    readingAloud = reading;
+    if (reading) {
+      ttsButton.innerHTML = '🔈 STOP';
+    } else {
+      readerPID = -1;
+      ttsButton.innerHTML = '🔊  TTS';
     }
+  },
+
+  async read(text) {
+    if (this.isReading()) {
+      this.stop();
+      this.setState(false);
+    } else {
+      this.setState(true);
+      await readApi(text);
+      this.setState(false);
+    }
+  },
+
+  stop() {
+    if (readerPID == -1) {
+      this.setState(false);
+    } else {
+      try {
+        process.kill(readerPID);
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+  },
 };
